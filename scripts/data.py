f0a78e930b5c78b3fff2a39192a0ae0acfbe14ab
Ruben Oliver Jonsman <rubj@itu.dk>
2024-10-11 11:18:34 +0200

    Now stores the data files on disc

--- scripts/data.py
+++ scripts/data.py
@@ -1,20 +1,35 @@
+import re
+from num2words import num2words
 import pandas as pd
 import torch.utils.data
 import os
 import torch
-from scripts.preprocessing import convert_sentence_to_char_sequence
+from scripts.preprocessing import convert_sentence_to_char_sequence, get_typoglycemia_modified_data
 
-def load_data(similarity_threshold: float, file_path: str) -> pd.DataFrame:
+
+def load_data(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df
+
+
+def generate_typoglycemia_data_file(similarity_threshold: float, file_path: str):
     df = pd.read_csv(file_path, sep="\t", names=["Hard", "Easy", "Similarity"])
     df = df[df["Similarity"] <= similarity_threshold]
-    df = df.drop_duplicates('Hard')
-    df = df.drop_duplicates('Easy')
-    return df
+    df['Hard'] = df['Hard'].apply(convert_numbers_to_words)
+    df['Easy'] = df['Easy'].apply(convert_numbers_to_words)
+
+    df = get_typoglycemia_modified_data(df)
+    df.reset_index(inplace=True, drop=True)
+    df.to_csv("../data/processed/sscorpus.csv", index=False)
+
+
+def convert_numbers_to_words(sentence):
+    return re.sub(r'\b\d+\b', lambda x: num2words(int(x.group())), sentence)
 
 
 def create_data_loader(df: pd.DataFrame, complexity: str = "Easy", batch_size: int = 16) -> torch.utils.data.DataLoader:
     sentence = convert_sentence_to_char_sequence(df[complexity])
-    typo_sentence = convert_sentence_to_char_sequence(df["typo_"+complexity])
+    typo_sentence = convert_sentence_to_char_sequence(df["typo_" + complexity])
 
     dataset = DataClass(typo_sentence, sentence)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
@@ -33,3 +48,8 @@ class DataClass(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         return self.typo_sentence[idx], self.target_sentence[idx]
+
+
+if __name__ == "__main__":
+    generate_typoglycemia_data_file(similarity_threshold=0.6, file_path="../data/raw/sscorpus")
+
